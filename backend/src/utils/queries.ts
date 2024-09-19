a3bab10b2e0e4fb800998feeb0dbe17cafe4abc3
squach28 <squach3443@gmail.com>
2024-09-19 12:23:59 -0700

    add commitTransaction for queries that cause change in db

--- backend/src/utils/queries.ts
+++ backend/src/utils/queries.ts
@@ -1,3 +1,4 @@
+import { QueryResult } from "pg";
 import { db } from "./db";
 
 const queries = {
@@ -5,6 +6,21 @@ const queries = {
     "INSERT INTO auth (email, password) VALUES ($1, $2) RETURNING uuid, email",
   getUserByEmail: "SELECT uuid, email FROM auth WHERE email = $1",
   getHashByEmail: "SELECT password FROM auth WHERE email = $1",
+};
+
+export const commitTransaction = async (
+  query: string,
+  values: string[]
+): Promise<QueryResult | null> => {
+  try {
+    await db.query("BEGIN");
+    const result = await db.query(query, values);
+    return result;
+  } catch (e) {
+    console.log(e);
+    await db.query("ROLLBACK");
+    return null;
+  }
 };
 
 export const getUserByEmail = async (
@@ -34,7 +50,13 @@ export const insertUser = async (
     if (userExists) {
       return null;
     }
-    const result = await db.query(queries.insertUser, [userEmail, hash]);
+    const result = await commitTransaction(queries.insertUser, [
+      userEmail,
+      hash,
+    ]);
+    if (result === null) {
+      return null;
+    }
     const { uuid, email } = result.rows[0];
     return { uuid, email };
   } catch (e) {
