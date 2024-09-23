593a99e331ffb944e495158089da896f4fe3ed94
squach28 <squach3443@gmail.com>
2024-09-23 09:38:44 -0700

    change menu in mobile to open a drawer

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -6,11 +6,59 @@ import {
   Typography,
   Link,
   IconButton,
+  Drawer,
+  ListItem,
+  List,
+  ListItemText,
+  Divider,
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import { Link as RouterLink } from "react-router-dom";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
+  const toggleDrawer = () => {
+    setOpen((prev) => !prev);
+  };
+
+  const DrawerList = () => {
+    return (
+      <Box sx={{ width: 200 }} role="presentation" onClick={toggleDrawer}>
+        <List>
+          <ListItem>
+            <ListItemText>Workout Hub</ListItemText>
+          </ListItem>
+          <Divider />
+          <ListItem>
+            <ListItemText>
+              <Link
+                to="/signup"
+                component={RouterLink}
+                sx={{ textDecoration: "none", color: "black" }}
+              >
+                Sign up
+              </Link>
+            </ListItemText>
+          </ListItem>
+          <ListItem>
+            <ListItemText>
+              <Link
+                to="/login"
+                component={RouterLink}
+                sx={{ textDecoration: "none", color: "black" }}
+              >
+                Log in
+              </Link>
+            </ListItemText>
+          </ListItem>
+        </List>
+      </Box>
+    );
+  };
+
+  // TODO: change the menu to a drawer
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" color="primary">
@@ -19,9 +67,12 @@ const Navbar = () => {
             Workout Hub
           </Typography>
           <Box display={{ xs: "block", md: "none" }} sx={{ ml: "auto" }}>
-            <IconButton color="secondary">
+            <IconButton color="secondary" onClick={toggleDrawer}>
               <MenuIcon sx={{ color: "white" }} />
             </IconButton>
+            <Drawer open={open} onClose={toggleDrawer} anchor="right">
+              {DrawerList()}
+            </Drawer>
           </Box>
           <Box display={{ xs: "none", md: "block" }} sx={{ ml: "auto" }}>
             <Stack direction="row" gap={2}>
